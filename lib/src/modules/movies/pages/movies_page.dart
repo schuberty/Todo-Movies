5f188d92804e34c7a0e63e7b8544480e8f5a6b5d
G. Schubert <schubertyi@gmail.com>
2022-06-14 22:45:02 -0300

    Implemented base of the default movies page

---
+++ lib/src/modules/movies/pages/movies_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoviesPage extends StatefulWidget {
+  const MoviesPage({Key? key}) : super(key: key);
+
+  @override
+  State<MoviesPage> createState() => _MoviesPageState();
+}
+
+class _MoviesPageState extends State<MoviesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
