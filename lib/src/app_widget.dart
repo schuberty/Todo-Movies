0c4854814914950b403f0983439917a7fe83eb45
G. Schubert <schubertyi@gmail.com>
2022-06-14 22:48:09 -0300

    Added shared modules to global providers, update app theme and routes

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_movies/src/shared/app_router.dart';
+import 'package:todo_movies/src/shared/app_theme.dart';
+import 'package:todo_movies/src/shared/modules/shared_modules.dart';
 
 class TodoMoviesApp extends StatelessWidget {
   const TodoMoviesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MultiProvider(
+      providers: [
+        ...sharedModules,
+      ],
+      child: MaterialApp(
+        theme: appTheme,
+        onGenerateRoute: AppRouter.onGenerateRoutes,
+      ),
     );
   }
 }
