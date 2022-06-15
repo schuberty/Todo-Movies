701faf40cf9768069b894232662b5663fe17a02e
G. Schubert <schubertyi@gmail.com>
2022-06-14 21:14:39 -0300

    Removed windows, linux, mac and web folders; Removed default counter app and implemented base app

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TodoMoviesApp extends StatelessWidget {
+  const TodoMoviesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
