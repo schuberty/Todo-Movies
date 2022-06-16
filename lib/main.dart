52ea54aa865c6a5ddf23076dc1d392f9aa8e0d19
G. Schubert <schubertyi@gmail.com>
2022-06-16 00:52:40 -0300

    Implement device orientation and system bars colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:todo_movies/src/app_widget.dart';
+import 'package:todo_movies/src/shared/app_constants.dart';
+
+import 'src/app_widget.dart';
 
 void main() {
   _initialConfigutarion();
@@ -8,11 +10,18 @@ void main() {
 }
 
 void _initialConfigutarion() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
   ]);
+
   SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent),
+    const SystemUiOverlayStyle(statusBarColor: cBackgroundColor),
   );
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: cBackgroundColor,
+  ));
 }
