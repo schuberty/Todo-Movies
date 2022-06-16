7173c149d8f1757633464642029b6d43b0355c22
G. Schubert <schubertyi@gmail.com>
2022-06-16 01:02:09 -0300

    Updated app theme texts, app bar and scaffold background color

--- lib/src/shared/app_theme.dart
+++ lib/src/shared/app_theme.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:todo_movies/src/shared/app_constants.dart';
 
-final appTheme = ThemeData(
+final appTheme = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: cBackgroundColor,
   useMaterial3: true,
+  appBarTheme: const AppBarTheme(
+    surfaceTintColor: Colors.transparent,
+  ),
+  textTheme: TextTheme(
+    displayLarge: const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+    ),
+    labelLarge: TextStyle(
+      color: Colors.grey.shade200,
+      fontSize: 16,
+    ),
+    labelMedium: TextStyle(
+      color: Colors.grey.shade300,
+      fontSize: 14,
+    ),
+  ),
 );
