ccc118fbc1a5adadde378a9f8fdbec3645a2b47d
G. Schubert <schubertyi@gmail.com>
2022-06-17 21:57:29 -0300

    Removed fonts from assets and added error thrown if api secret is empty

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:todo_movies/src/shared/app_constants.dart';
+import 'package:todo_movies/src/shared/app_secrets.dart';
 
 import 'src/app_widget.dart';
 
 void main() {
+  _checkConstants();
   _initialConfigutarion();
   runApp(const TodoMoviesApp());
+}
+
+void _checkConstants() {
+  if (apiKeyMovieDB.isEmpty) {
+    throw Exception(
+      'API Key for The Movie Database API must not be empty! Check the README file for more details.',
+    );
+  }
 }
 
 void _initialConfigutarion() {
